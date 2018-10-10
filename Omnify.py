a8b66596e5c563b17881087c76e5915b272250c1
Tenumbra <leet-shadow@hotmail.com>
2018-10-10 14:22:38 -0400

    Updated with statistical preference

--- Omnify.py
+++ Omnify.py
@@ -6,48 +6,74 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 #Stored variables, to change depending on user
 username = 'omnitenebris'
-scope = 'user-library-read'
-redirectUri = 'http://www.andrewnovac.com'
+scope = ['user-library-read', 'user-top-read']
+redirectUri = 'http://www.andrewnovac.com/journey'
 clientId = '25cdc49237b348c297dff633d59bb46f'
 clientSecret = 'bcef5ca156264f7085c788713e8719ec'
 
-#Spotify Authorization 
-token = util.prompt_for_user_token(username, scope,
+def songs():
+    #Get the authorization
+    tokenSong = util.prompt_for_user_token(username, scope[0],
     client_id= clientId,
     client_secret = clientSecret,
     redirect_uri = redirectUri)
 
-#Get a list of users saved tracks
-sp = spotipy.Spotify(auth=token)
-results = sp.current_user_saved_tracks()
+    #Get a list of users saved tracks
+    sp = spotipy.Spotify(auth=tokenSong)
+    results = sp.current_user_saved_tracks()
 
-#Add the IDs of the tracks and artists
-Tracks = []
-Artists = []
-for item in results['items']:
-    track = item['track']
-    Tracks.append(track['id'])
-    Artists.append(track['artists'][0]['id'])
+    #Add the IDs of the tracks and artists
+    Tracks = []
+    Artists = []
+    for item in results['items']:
+        track = item['track']
+        Tracks.append(track['id'])
+        Artists.append(track['artists'][0]['id'])
 
-#Send out a request with the track seeds to the spotify API
-seedTracks = ('%2C').join(Tracks[:5])
-r = requests.get("https://api.spotify.com/v1/recommendations?market=CA&seed_tracks=" + seedTracks + "&min_energy=0.4&min_popularity=50",
-    headers = {'Content-Type': 'application/json',
-        'Accept': 'text/javascript',
-        "Authorization": "Bearer BQC6cJ9s2jfPiQ2kAE-4I6DWdNKiKcdO7CF6REvsMnnJgn3eXiYFdhIMm8k8OSq9j2cZPwUTGO_9dv2SQU2l8U65EXVWRbZmQCLQAp7yxywTc3kJRFZ_BcX7bU41dgRawsjeree4ozT6iTAXjDDCZ96y5eNrfAtBYA"})
+    #Send out a request with the track seeds to the spotify API
+    seedTracks = ('%2C').join(Tracks[:5])
+    r = requests.get("https://api.spotify.com/v1/recommendations?market=CA&seed_tracks=" + seedTracks + "&min_energy=0.4&min_popularity=50",
+        headers = {'Content-Type': 'application/json',
+            'Accept': 'text/javascript',
+            "Authorization": "Bearer " + tokenSong})
 
-#Output the recommended tracks
-i = 0
-print("\nHere are your recommended artists: \n")
-while i < 5:
-    print(json.loads(r.text)['tracks'][i]['name'] + " - " +  json.loads(r.text)['tracks'][i]['artists'][0]['name'])
-    i += 1
-print('\n')
+    #Output the recommended tracks
+    i = 0
+    print("\nHere are some recommended songs: \n")
+    while i < 5:
+        print(json.loads(r.text)['tracks'][i]['name'] + " - " +  json.loads(r.text)['tracks'][i]['artists'][0]['name'])
+        i += 1
 
-#Send out a request with the album seeds to the spotify API
-seedArtists = ('%2C').join(Artists[:5])
-r = requests.get("https://api.spotify.com/v1/recommendations?market=CA&seed_artists=" + seedArtists + "&min_energy=0.4&min_popularity=50",
-    headers = {'Content-Type': 'application/json',
-        'Accept': 'text/javascript',
-        "Authorization": "Bearer BQA4JqQKbqrWKAwj5lOvZ-eacGQu9YY13zNE9qUHhSRCC6cesGJN60HxsscFRSwRj4npO3iBa13nMrs9FN1WxrSBpFtl4WI4iiWKXMGc2JVymhHHw9Q-ugPgNv6T5IM0TH5xnpVIKh8chI7dzHKASNP24XqSv-A"})
-#print(r.text)
+    #Send out a request with the album seeds to the spotify API
+    seedArtists = ('%2C').join(Artists[:5])
+    r = requests.get("https://api.spotify.com/v1/recommendations?market=CA&seed_artists=" + seedArtists + "&min_energy=0.4&min_popularity=50",
+        headers = {'Content-Type': 'application/json',
+            'Accept': 'text/javascript',
+            "Authorization": "Bearer " + tokenSong})
+    #print(r.text)
+
+
+def statistics():
+    tokenStats = util.prompt_for_user_token(username, scope[1],
+    client_id= clientId,
+    client_secret = clientSecret,
+    redirect_uri = redirectUri)
+
+    r = requests.get("https://api.spotify.com/v1/me/top/artists",
+        headers = {'Content-Type': 'application/json',
+            'Accept': 'text/javascript',
+            "Authorization": "Bearer " + tokenStats})
+    print(r.text)
+
+#Ask for user input regarding what to do
+print("What would you like to do today?")
+while True:
+    userRespone = input('Show my spotify statistics or recommend songs for me (stats/songs): ')
+    if userRespone.lower() == "stats":
+        statistics()
+        break
+    elif userRespone.lower() == "songs":
+        songs()
+        break
+    else:
+        print('Please type either "statistics" or "songs"\n')
