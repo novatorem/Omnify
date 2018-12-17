4318460bda741f994fd6b7a1665846b7ee9c13ef
Tenumbra <leet-shadow@hotmail.com>
2018-12-17 00:13:23 -0500

    Added artists and rearranged code

--- Omnify.py
+++ Omnify.py
@@ -18,10 +18,66 @@ def main():
     global username
     username = userRespone
     print("\nHi " + username + "!")
-    songs()
-    statistics()
+    artists()
+    recommendations()
 
-def songs():
+def artists(genres = True, artists = True, popularity = True):
+    #Connect to the API
+    tokenStats = util.prompt_for_user_token('omnitenebris', scope,
+    client_id= clientId,
+    client_secret = clientSecret,
+    redirect_uri = redirectUri)
+    r = requests.get("https://api.spotify.com/v1/me/top/artists",
+        headers = {'Content-Type': 'application/json',
+            'Accept': 'text/javascript',
+            "Authorization": "Bearer " + tokenStats})
+    
+    #Get popularity of favorite artists
+    if popularity == True:
+        popularity = []
+        followers = []
+        for artist in json.loads(r.text)['items']:
+            popularity.append(artist['popularity'])
+        for artist in json.loads(r.text)['items']:
+            followers.append(artist['followers']['total'])
+        popMeter = int(sum(popularity) / float(len(popularity)))
+        folMeter = int(sum(followers) / float(len(followers)))
+        ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(math.floor(n/10)%10!=1)*(n%10<4)*n%10::4])
+        print("\nYour favorite artists have an average of " + str(folMeter) +
+                " followers\nRanked in the " +
+                ordinal(popMeter) + " percentile of artists, impressive!\n")
+    
+    if artists == True:
+        artists = []
+        for artist in json.loads(r.text)['items']:
+            artists.append(artist['name'])
+        print("The following are your top ten artists:")
+        for i in range(0, 10):
+            print("\t" + str(i + 1) + ". " + artists[i])
+        print("")
+
+    #Get users most listened to genres and return them
+    if genres == True:
+        print("Your favorite music falls within the following genres:")
+        genres = []
+        for artist in json.loads(r.text)['items']:
+            genres.extend(artist['genres'])
+        userGenres = ("\t- " + "\n\t- ".join(
+            list
+                (dict.fromkeys(
+                    sorted(
+                        genres,
+                        key = genres.count,
+                        reverse=True)
+                    )
+                )[:10]
+            )
+        )
+        print(userGenres.title() + "\n")
+    
+    #print(r.text)
+
+def recommendations():
     #Get the authorization
     tokenSong = util.prompt_for_user_token('omnitenebris', scope,
         client_id = clientId,
@@ -49,7 +105,7 @@ def songs():
 
     #Output the recommended tracks
     i = 0
-    print("\nHere are some recommended songs: \n")
+    print("Below are some recommended songs based on your more recent interests: \n")
     while i < 5:
         track = json.loads(r.text)['tracks'][i]['name']
         artist = json.loads(r.text)['tracks'][i]['artists'][0]['name']
@@ -63,54 +119,6 @@ def songs():
         headers = {'Content-Type': 'application/json',
             'Accept': 'text/javascript',
             "Authorization": "Bearer " + tokenSong})
-    #print(r.text)
-
-def statistics(genres = True, popularity = True):
-    #Connect to the API
-    tokenStats = util.prompt_for_user_token('omnitenebris', scope,
-    client_id= clientId,
-    client_secret = clientSecret,
-    redirect_uri = redirectUri)
-    r = requests.get("https://api.spotify.com/v1/me/top/artists",
-        headers = {'Content-Type': 'application/json',
-            'Accept': 'text/javascript',
-            "Authorization": "Bearer " + tokenStats})
-    
-    #Get popularity of favorite artists
-    if popularity == True:
-        popularity = []
-        followers = []
-        for artist in json.loads(r.text)['items']:
-            popularity.append(artist['popularity'])
-        for artist in json.loads(r.text)['items']:
-            followers.append(artist['followers']['total'])
-        popMeter = int(sum(popularity) / float(len(popularity)))
-        folMeter = int(sum(followers) / float(len(followers)))
-        ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(math.floor(n/10)%10!=1)*(n%10<4)*n%10::4])
-        print("\nYour favorite artists have an average of " + str(folMeter) +
-                " followers\nRanked in the top " +
-                ordinal(popMeter) + " percentile of artists, impressive!\n")
-
-    #Get users most listened to genres and return them
-    if genres == True:
-        print("Your favorite music falls within the following genres:")
-        genres = []
-        for artist in json.loads(r.text)['items']:
-            genres.extend(artist['genres'])
-        userGenres = ("\t- " + "\n\t- ".join(
-            list
-                (dict.fromkeys(
-                    sorted(
-                        genres,
-                        key = genres.count,
-                        reverse=True)
-                    )
-                )[:10]
-            )
-        )
-        print(userGenres.title() + "\n")
-    
-    #print(r.text)
 
 if __name__ == "__main__":
     main()
